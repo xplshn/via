abcc4a2a17c7dcbc4a4330b644ca0a233efb3dd1
xplshn <xplshn@murena.io>
2024-10-13 03:41:21 -0300

    [+]

--- via-nohist.go
+++ via-nohist.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,124 +10,151 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-var (
-	verbose bool
-	port    int
-	password = "secret"
-)
-
-type PubSub struct {
-	mu       sync.Mutex
-	clients  map[string]chan string
-	messages chan string
+type Server struct {
+	topics   map[string]*Topic
+	mu       sync.RWMutex
+	verbose  bool
+	port     string
 }
 
-func NewPubSub() *PubSub {
-	return &PubSub{
-		clients:  make(map[string]chan string),
-		messages: make(chan string),
+type Topic struct {
+	subscribers map[string]chan string
+	password    string
+	mu          sync.RWMutex
+}
+
+type StatusResponse struct {
+	Topics []string `json:"topics"`
+}
+
+func NewServer(verbose bool) *Server {
+	return &Server{
+		topics:  make(map[string]*Topic),
+		verbose: verbose,
 	}
 }
 
-func (ps *PubSub) Subscribe(clientID string) chan string {
-	ch := make(chan string, 1)
-	ps.mu.Lock()
-	ps.clients[clientID] = ch
-	ps.mu.Unlock()
-	return ch
-}
+func (s *Server) Publish(topic string, message string) {
+	s.mu.RLock()
+	t, exists := s.topics[topic]
+	s.mu.RUnlock()
 
-func (ps *PubSub) Unsubscribe(clientID string) {
-	ps.mu.Lock()
-	delete(ps.clients, clientID)
-	ps.mu.Unlock()
-}
-
-func (ps *PubSub) Publish(msg string) {
-	ps.messages <- msg
-}
-
-func (ps *PubSub) Run() {
-	for msg := range ps.messages {
-		ps.mu.Lock()
-		for _, ch := range ps.clients {
-			ch <- msg
+	if exists {
+		t.mu.RLock()
+		defer t.mu.RUnlock()
+		for _, subscriber := range t.subscribers {
+			subscriber <- message
 		}
-		ps.mu.Unlock()
 	}
+}
+
+func (s *Server) Subscribe(topic, password string) (chan string, error) {
+	s.mu.RLock()
+	t, exists := s.topics[topic]
+	s.mu.RUnlock()
+
+	if !exists || t.password != password {
+		return nil, fmt.Errorf("subscription failed: topic not found or incorrect password")
+	}
+
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	ch := make(chan string)
+	t.subscribers[password] = ch
+
+	return ch, nil
+}
+
+func (s *Server) CreateTopic(name, password string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.topics[name] = &Topic{
+		subscribers: make(map[string]chan string),
+		password:    password,
+	}
+}
+
+func (s *Server) StatusHandler(ctx *fasthttp.RequestCtx) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	status := StatusResponse{Topics: make([]string, 0, len(s.topics))}
+	for topic := range s.topics {
+		status.Topics = append(status.Topics, topic)
+	}
+
+	if s.verbose {
+		log.Println("Status requested")
+	}
+
+	ctx.SetContentType("application/json")
+	json.NewEncoder(ctx).Encode(status)
+}
+
+func (s *Server) PublishHandler(ctx *fasthttp.RequestCtx) {
+	topic := ctx.UserValue("topic").(string)
+	message := string(ctx.PostBody())
+
+	s.Publish(topic, message)
+
+	if s.verbose {
+		log.Printf("Published message to topic '%s': %s\n", topic, message)
+	}
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+}
+
+func (s *Server) SubscribeHandler(ctx *fasthttp.RequestCtx) {
+	topic := ctx.UserValue("topic").(string)
+	password := ctx.QueryArgs().Peek("password")
+
+	ch, err := s.Subscribe(topic, string(password))
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusUnauthorized)
+		ctx.SetBodyString(err.Error())
+		return
+	}
+
+	if s.verbose {
+		log.Printf("Subscriber added for topic '%s'\n", topic)
+	}
+
+	go func() {
+		for msg := range ch {
+			_, _ = ctx.Write([]byte(msg + "\n"))
+		}
+	}()
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+}
+
+func (s *Server) SetupRoutes() {
+	fasthttp.ListenAndServe(":"+s.port, func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/status":
+			s.StatusHandler(ctx)
+		case "/publish":
+			s.PublishHandler(ctx)
+		case "/subscribe":
+			s.SubscribeHandler(ctx)
+		default:
+			ctx.SetStatusCode(fasthttp.StatusNotFound)
+		}
+	})
 }
 
 func main() {
-	flag.BoolVar(&verbose, "verbose", false, "Enable verbose mode")
-	flag.IntVar(&port, "port", 8080, "Port to listen on")
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	port := flag.String("port", "8080", "Port to run the server on")
 	flag.Parse()
 
-	ps := NewPubSub()
-	go ps.Run()
+	server := NewServer(*verbose)
+	server.CreateTopic("testTopic", "secret")
 
-	requestHandler := func(ctx *fasthttp.RequestCtx) {
-		switch string(ctx.Path()) {
-		case "/subscribe":
-			handleSubscribe(ctx, ps)
-		case "/publish":
-			handlePublish(ctx, ps)
-		case "/status":
-			handleStatus(ctx)
-		default:
-			ctx.Error("Not Found", fasthttp.StatusNotFound)
-		}
+	if *verbose {
+		log.Printf("Starting server on port %s...\n", *port)
 	}
 
-	log.Printf("Starting server on port %d\n", port)
-	if err := fasthttp.ListenAndServe(fmt.Sprintf(":%d", port), requestHandler); err != nil {
-		log.Fatalf("Error in ListenAndServe: %s", err)
-	}
-}
-
-func handleSubscribe(ctx *fasthttp.RequestCtx, ps *PubSub) {
-	if string(ctx.QueryArgs().Peek("password")) != password {
-		ctx.Error("Unauthorized", fasthttp.StatusUnauthorized)
-		return
-	}
-
-	clientID := string(ctx.QueryArgs().Peek("clientID"))
-	if clientID == "" {
-		ctx.Error("Bad Request", fasthttp.StatusBadRequest)
-		return
-	}
-
-	ch := ps.Subscribe(clientID)
-	defer ps.Unsubscribe(clientID)
-
-	ctx.SetBody([]byte("Subscribed"))
-	ctx.SetStatusCode(fasthttp.StatusOK)
-
-	for msg := range ch {
-		if verbose {
-			log.Printf("Sending message to %s: %s\n", clientID, msg)
-		}
-		ctx.Write([]byte(msg))
-	}
-}
-
-func handlePublish(ctx *fasthttp.RequestCtx, ps *PubSub) {
-	if string(ctx.QueryArgs().Peek("password")) != password {
-		ctx.Error("Unauthorized", fasthttp.StatusUnauthorized)
-		return
-	}
-
-	msg := string(ctx.PostBody())
-	if msg == "" {
-		ctx.Error("Bad Request", fasthttp.StatusBadRequest)
-		return
-	}
-
-	ps.Publish(msg)
-	ctx.SetBody([]byte("Published"))
-	ctx.SetStatusCode(fasthttp.StatusOK)
-}
-
-func handleStatus(ctx *fasthttp.RequestCtx) {
-	ctx.SetBody([]byte("OK"))
-	ctx.SetStatusCode(fasthttp.StatusOK)
+	server.port = *port
+	server.SetupRoutes()
 }
